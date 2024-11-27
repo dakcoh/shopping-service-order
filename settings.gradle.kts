94bf4669a281e075492f9fb9ee3c3743de0e5435
dakcoh <dakcoh@gmail.com>
2024-11-27 22:59:27 +0900

    update : implementation & reduce project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,11 @@
 rootProject.name = "shopping-service-order"
 // 각 서브프로젝트를 포함
 include("order-service")
-//include("payment-service")
+include("payment-service")
 //include("product-service")
 //include("shipping-service")
 //include("notification-service")
 //include("customer-service")
-//include("api-gateway")
+include("api-gateway")
 //include("cart-service")
+include("shared-dto")
