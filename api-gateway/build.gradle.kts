17956b48890c32386a699774b0caa0432f3b1894
dakcoh <dakcoh@gmail.com>
2025-01-23 17:13:35 +0900

    feature/drop jwt & 불필요 코드 제거 작업

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -8,25 +8,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // jwt// https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-api
-    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
-    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-impl
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
-    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-jackson
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
-
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-redis-reactive
-    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive:3.3.6")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive:3.4.1")
+
     // https://mvnrepository.com/artifact/io.lettuce/lettuce-core
     implementation("io.lettuce:lettuce-core:6.5.1.RELEASE")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
-
-    // https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-gateway
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.6")
 
     // 추가적으로 필요한 의존성 정의
     runtimeOnly("com.h2database:h2") // Order 서비스에서 H2 DB를 사용
